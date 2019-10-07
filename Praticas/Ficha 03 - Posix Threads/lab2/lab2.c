df271851129c5bc7c1020697e5d70e8b35fefa71
unknown <tiago.o.caetano@gmail.com>
2019-10-07 23:43:21 +0100

    Inicio da ficha03

--- Praticas/Ficha 03 - Posix Threads/lab2/lab2.c
+++ Praticas/Ficha 03 - Posix Threads/lab2/lab2.c
@@ -9,12 +9,25 @@
 
 int main (int argc, char *argv[])
 {	
-	
+	char comando[100];
+	int result;
 	/* Silencia os warnings */
 	(void)argc;
 	(void)argv;
 
-	 
 
+	printf("Nome do programa: %s\n", argv[0]);
+	printf("PID do processo atual: %d\n",getpid());
+	
+	sprintf(comando,"ps -eLF |  grep %s | grep -v grep | tr -s ' ' | cut -f6 -d' ' | tail -n1", argv[0]);
+	
+	printf("Numero de threads do processo atual: ");
+	fflush(stdout);
+	result = system(comando);
+
+	if(result < 0){
+		ERROR(1, "Erro na execução do comando \n");
+	}
+	
 	exit(0);
 }
