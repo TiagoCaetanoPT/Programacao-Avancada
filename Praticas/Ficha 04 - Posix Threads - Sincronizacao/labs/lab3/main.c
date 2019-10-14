895fed7487641fbc67a12b5a4eab33af7cb5e2a3
unknown <tiago.o.caetano@gmail.com>
2019-10-14 17:54:08 +0100

    Ficha4 Lab4

--- Praticas/Ficha 04 - Posix Threads - Sincronizacao/labs/lab3/main.c
+++ Praticas/Ficha 04 - Posix Threads - Sincronizacao/labs/lab3/main.c
@@ -18,15 +18,15 @@ void *thread_1(void *arg);
 void *thread_2(void *arg);
 
 /* Estrutura a 'passar' às threads */
-typedef struct 
-{	
+typedef struct
+{
     pthread_mutex_t mutex;
-    
+    pthread_cond_t cond;
 }thread_params_t;
 
 
 /*  main  */
-int main(int argc, char *argv[]) 
+int main(int argc, char *argv[])
 {
     pthread_t t1, t2;
     thread_params_t thread_params;
@@ -37,24 +37,32 @@ int main(int argc, char *argv[])
 	if ((errno = pthread_mutex_init(&thread_params.mutex, NULL)) != 0)
 		ERROR(C_ERRO_MUTEX_INIT, "pthread_mutex_init() failed!");
 
+	/* Inicialização da variavel de condicao */
+	if ((errno = pthread_cond_init(&thread_params.cond, NULL)) != 0)
+		ERROR(C_ERRO_CONDITION_INIT, "pthread_mutex_init() failed!");
+
     /* Cria thread_1 */
 	if ((errno = pthread_create(&t1, NULL, thread_1, &thread_params)) != 0)
 		ERROR(C_ERRO_PTHREAD_CREATE, "pthread_create() failed!");
-	
+
     /* Cria thread_2 */
 	if ((errno = pthread_create(&t2, NULL, thread_2, &thread_params)) != 0)
 		ERROR(C_ERRO_PTHREAD_CREATE, "pthread_create() failed!");
 
     /* Espera que as duas (2) threads terminem */
 	if ((errno = pthread_join(t1, NULL)) != 0)
-		ERROR(C_ERRO_PTHREAD_JOIN, "pthread_join() failed!");	
+		ERROR(C_ERRO_PTHREAD_JOIN, "pthread_join() failed!");
 	if ((errno = pthread_join(t2, NULL)) != 0)
 		ERROR(C_ERRO_PTHREAD_JOIN, "pthread_join() failed!");
-						
+
 	/* Destroi o mutex */
 	if ((errno = pthread_mutex_destroy(&thread_params.mutex)) != 0)
-		ERROR(C_ERRO_MUTEX_DESTROY, "pthread_mutex_destroy() failed!");			
-			
+		ERROR(C_ERRO_MUTEX_DESTROY, "pthread_mutex_destroy() failed!");
+
+	/* Destruição da variavel de condicao */
+	if ((errno = pthread_cond_destroy(&thread_params.cond)) != 0)
+		ERROR(C_ERRO_CONDITION_DESTROY, "pthread_mutex_destroy() failed!");
+
     return 0;
 }
 
@@ -62,10 +70,10 @@ int main(int argc, char *argv[])
 /* Thread 1 */
 void *thread_1(void *arg)
 {
-	(void)arg;  
-	
-	printf("Thread_1\n");	
-		
+	(void)arg;
+
+	printf("Thread_1\n");
+
     return NULL;
 }
 
@@ -74,8 +82,8 @@ void *thread_1(void *arg)
 void *thread_2(void *arg)
 {
 	(void)arg;
-   
+
 	printf("Thread_2\n");
-	   
+
     return NULL;
 }
