15818c6e3f3bd439fc7d51541dc4c09b6fded78d
unknown <tiago.o.caetano@gmail.com>
2019-10-15 21:34:29 +0100

    Ficha01 - lab1 e 2

--- Praticas/Ficha 01 - Precessos/ficha1_labs/lab2/lab2.c
+++ Praticas/Ficha 01 - Precessos/ficha1_labs/lab2/lab2.c
@@ -7,8 +7,8 @@ int main (int argc, char *argv[])
 	/* Silencia os warnings */
 	(void)argc;
 	(void)argv;
-
-	printf ("O meu PID e':%d \n\n", getpid ());
-	printf ("O PID do meu pai e':%d\n\n", getppid ());
+    
+    printf ("O meu PID e':%d \n\n", getpid ());
+    printf ("O PID do meu pai e':%d\n\n", getppid ());
 	return 0;
 }
